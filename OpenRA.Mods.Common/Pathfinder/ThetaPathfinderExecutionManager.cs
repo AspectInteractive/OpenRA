af582eade3ba8851c5c7895cdadf19d7c599aa5a
AspectInteractive <vortex01@gmail.com>
2021-12-05 14:21:32 +1100

    This is now a working version of the execution manager. Also confirmed this works in multiplayer. Attack move still seems a little imperfect (the occasional unit moving too far forward), though all the core functionality works at this point.

--- OpenRA.Mods.Common/Pathfinder/ThetaPathfinderExecutionManager.cs
+++ OpenRA.Mods.Common/Pathfinder/ThetaPathfinderExecutionManager.cs
@@ -1,33 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using OpenRA.Mods.Common.Pathfinder;
+using OpenRA.Primitives;
 using OpenRA.Traits;
 
-namespace OpenRA.Mods.Common.Pathfinder
+namespace OpenRA.Mods.Common.Traits
 {
 	[TraitLocation(SystemActors.World)]
 	[Desc("Manages the queuing and prioritisation of Theta Pathfinder calculations, to ensure the computer is not overloded.")]
 
-	public class ActorID
-	{
-		public uint ID;
-		public ActorID(uint actorID) { ID = actorID; }
-	}
-
-	public class ThetaPathfinderExecutionManagerInfo : TraitInfo<ThetaPathfinderExecutionManagerInfo> { }
+	public class ThetaPathfinderExecutionManagerInfo : TraitInfo<ThetaPathfinderExecutionManager> { }
 	public class ThetaPathfinderExecutionManager : ITick
 	{
-		public Dictionary<ActorID, ThetaStarPathSearch> ThetaPathfinders = new Dictionary<ActorID, ThetaStarPathSearch>();
-		void ITick.Tick(Actor world)
+		int maxCurrExpansions = 200;
+
+		public ThetaPathfinderExecutionManager() { }
+
+		public Dictionary<Actor, ThetaStarPathSearch> ThetaPathfinders = new Dictionary<Actor, ThetaStarPathSearch>();
+		void ITick.Tick(Actor world) { Tick(); }
+
+		public void AddPF(Actor actor, ThetaStarPathSearch thetaPF)
 		{
-			Tick();
+			if (!ThetaPathfinders.ContainsKey(actor))
+				ThetaPathfinders.Add(actor, thetaPF);
+			ThetaPathfinders[actor] = thetaPF;
 		}
 
 		private void Tick()
 		{
-			foreach (var thetaPF in ThetaPathfinders)
-				if (thetaPF.Value.pathFound == false)
-					thetaPF.Value.Expand();
+			foreach (var (_, thetaPF) in ThetaPathfinders)
+				if (thetaPF.pathFound == false)
+					thetaPF.Expand((int)Fix64.Ceiling((Fix64)maxCurrExpansions / (Fix64)ThetaPathfinders.Count));
 		}
 	}
 }
