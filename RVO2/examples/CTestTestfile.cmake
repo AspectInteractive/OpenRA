7caa2660e5d3b10e7aa858aabbb195128fe8dcd6
AspectInteractive <vortex01@gmail.com>
2022-02-11 01:03:53 +1100

    Removed RVO2 as not using.

--- RVO2/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/Jire/RVO2/examples
-# Build directory: G:/Jire/RVO2/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
